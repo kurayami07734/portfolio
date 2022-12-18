134c59ee6fccb8074f3b0d9226e35875a8cd6476
Aditya Ghidora <adityaghidora@gmail.com>
2022-12-18 09:45:34 +0530

    added basic logo and links

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,26 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <p class="greeting">Hello, there</p>
+    <p class="intro">I am Aditya Ghidora</p>
+    <p>
+      This website is under construction. While you wait, you contact me from
+      below links
+    </p>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
+  <div>
+    <a href="https://www.linkedin.com/in/aditya-ghidora/">LinkedIn</a>
+    <a href="mailto:contact@ghidora.tech">Mail</a>
   </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    reactivity rocks
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  .greeting {
+    font-size: 25px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+  .intro {
+    font-size: 35px;
   }
 </style>
